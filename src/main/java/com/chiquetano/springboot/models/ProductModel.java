bd1c07166d1d38ed23bb6a84a227759d75b3d94e
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-27 03:33:29 -0300

    fix: using @Column annotation

--- src/main/java/com/chiquetano/springboot/models/ProductModel.java
+++ src/main/java/com/chiquetano/springboot/models/ProductModel.java
@@ -7,37 +7,43 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
-@Table(name = "TB PRoDUCTS")
+@Table(name = "TB_PRODUCTS")
 public class ProductModel implements Serializable {
-    private static final long serialVersionUID = 1l;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_product", updatable = false, nullable = false)
     private UUID idProduct;
-    private String name;
+
+    @Column(name = "product_value", precision = 38, scale = 2)
     private BigDecimal value;
 
-    public void setIdProduct(UUID idProduct) {
-        this.idProduct = idProduct;
-    }
+    @Column(name = "name", length = 255)
+    private String name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
 
     public UUID getIdProduct() {
         return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public BigDecimal getValue() {
         return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
     }
 }
