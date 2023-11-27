60a6e40ba7f161c3a194deab41b1560303a5e960
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-27 03:13:05 -0300

    feat: create route POST /products

--- src/main/java/com/chiquetano/springboot/controllers/ProductController.java
+++ src/main/java/com/chiquetano/springboot/controllers/ProductController.java
@@ -1,7 +1,15 @@
 package com.chiquetano.springboot.controllers;
-g
+
+import com.chiquetano.springboot.dtos.ProductRecordDto;
+import com.chiquetano.springboot.models.ProductModel;
 import com.chiquetano.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +18,10 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 }
