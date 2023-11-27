dad56d1bcc82762ecd51e286c7e3437c893cae75
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-27 03:12:08 -0300

    feat: create ProductController

---
+++ src/main/java/com/chiquetano/springboot/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package com.chiquetano.springboot.controllers;
+g
+import com.chiquetano.springboot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+}
