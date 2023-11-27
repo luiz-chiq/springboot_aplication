732cac8b2b11c16362898ddf2ecff800400cd6b3
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-27 03:52:57 -0300

    feat: create route PUT /products/{id}

--- src/main/java/com/chiquetano/springboot/controllers/ProductController.java
+++ src/main/java/com/chiquetano/springboot/controllers/ProductController.java
@@ -38,4 +38,16 @@ public class ProductController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(product.get());
     }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProducts(@PathVariable(value="id") UUID id,
+                                                 @RequestBody @Valid ProductRecordDto productRecordDto){
+        Optional<ProductModel> product = productRepository.findById(id);
+        if(product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        var productModel = product.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
 }
