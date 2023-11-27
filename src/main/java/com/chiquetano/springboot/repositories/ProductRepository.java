20bd88aaad09514e1b38e3afca3767d10d06348c
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-27 03:03:36 -0300

    feat: create ProductRepository

---
+++ src/main/java/com/chiquetano/springboot/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.chiquetano.springboot.repositories;
+
+import com.chiquetano.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+//@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
