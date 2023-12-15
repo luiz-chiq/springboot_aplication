2c4b6e5e4259914a46508cd536bbb6d5c68478b7
luiz-chiq <chiquetanoluiz@gmail.com>
2023-12-15 07:09:51 -0300

    build: create a frontend application

---
+++ frontend/app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductForm from './components/createProductCard/createProduct';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductForm></ProductForm>
+    </div>
+  );
+}
+
+export default App;
